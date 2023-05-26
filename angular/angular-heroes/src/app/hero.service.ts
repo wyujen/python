9ce2e5a616e79aa15935e007d1dc9978058cc52f
wyujen <129763210+wyujen@users.noreply.github.com>
2023-05-26 14:05:19 +0800

    1

--- angular/angular-heroes/src/app/hero.service.ts
+++ angular/angular-heroes/src/app/hero.service.ts
@@ -61,4 +61,33 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     )
   }
+
+  addHero(hero: Hero): Observable<Hero>{
+    return this.http.post<Hero>(this.heroesUrl, hero,this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id={newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_=>this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(` ${this.heroesUrl}/?name=${term} `).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`):
+        this.log(`no heroes ,matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes',[]))
+    );
+    
+  }
 }
