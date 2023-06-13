8fed42f65532b1d302311a8634ceccb55f08cc63
wyujen <129763210+wyujen@users.noreply.github.com>
2023-06-13 13:12:02 +0800

    0609

--- angular/test/src/app/tagmovie/tagmovie.component.ts
+++ angular/test/src/app/tagmovie/tagmovie.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TagserveService } from './tagserve.service';
 import { Movie, Tag, MovieOutput, TagOutput } from './movietag.interface';
 import { TAGS, MOVIES } from './movieData'
-import { Observable, combineLatest} from 'rxjs';
-import { map, filter, toArray, switchMap, mergeMap, mergeAll } from 'rxjs/operators';
+import { Observable, combineLatest, concat, forkJoin, zip} from 'rxjs';
+import { map, filter, toArray, switchMap, mergeMap, mergeAll, withLatestFrom, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tagmovie',
@@ -26,7 +26,42 @@ export class TagmovieComponent implements OnInit {
     this._movies$ = this._tagServe._movies$;
     this._tags$ = this._tagServe._tags$ 
 
-    this.moviesOutput = combineLatest([this._movies$, this._tags$]).pipe(
+    // 1.
+    // this.moviesOutput = combineLatest([this._movies$, this._tags$]).pipe(
+    //   map(([movies, tags]) => {
+    //     return movies.map(movie => ({
+    //       ...movie,
+    //       tagNames: movie.tagid.map(tagid => 
+    //         (tags.find(tag => tag.id === tagid) || {}).name)
+    //     }));
+    //   }),
+    // )
+
+    // 2.
+    // this.moviesOutput = this._movies$.pipe(
+    //   withLatestFrom(this._tags$),
+    //   map(([movies, tags]) => {
+    //     return movies.map(movie => ({
+    //       ...movie,
+    //       tagNames: movie.tagid.map(tagid => 
+    //         (tags.find(tag => tag.id === tagid) || {}).name)
+    //     }));
+    //   }),
+    // )
+
+    // 3.
+    // this.moviesOutput = zip(this._movies$, this._tags$).pipe(
+    //   map(([movies, tags]) => {
+    //     return movies.map(movie => ({
+    //       ...movie,
+    //       tagNames: movie.tagid.map(tagid => 
+    //         (tags.find(tag => tag.id === tagid) || {}).name)
+    //     }));
+    //   }),
+    // )
+
+    // 4
+    this.moviesOutput = forkJoin([this._movies$, this._tags$]).pipe(
       map(([movies, tags]) => {
         return movies.map(movie => ({
           ...movie,
@@ -35,6 +70,8 @@ export class TagmovieComponent implements OnInit {
         }));
       }),
     )
+
+    
   }
 
   
